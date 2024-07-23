ad7da4da9d2e29775776a8f0c6ac6c6727afd9e0
tomvanenckevort-hp <tomvanenckevort-hp@users.noreply.github.com>
2024-07-23 15:51:30 +0000

    Automated build

--- dist/assets/index.hbs-B9xfTPO7.js
+++
@@ -1,1 +0,0 @@
-import"./main-BijWZemd.js";customElements.define("component-card",class extends HTMLElement{constructor(){super();const e=document.getElementById("component-card-template").content;this.appendChild(e.cloneNode(!0))}});
